206df0a3297bec0b7975b6d9fcfb1a0c3ab0fa0f
Yxz12-ye <3078961710@qq.com>
2025-12-26 20:04:51 +0800

    添加新文章, 为一些文章添加预览

---
+++ assets/about_recently_1.ZyhnZdgd.js
@@ -0,0 +1,133 @@
+import{_ as d}from"./ValaxyMain.vue_vue_type_style_index_0_lang.BVmUPrIi.js";import"./chunks/@vueuse/motion.CfOaydB9.js";import{e as m,u as b,a as g}from"./chunks/vue-router.CM5PG3tg.js";import{aa as E,ap as l,ag as p,af as n,ae as h,ai as s,F as T,ab as P,Z as A}from"./framework.Dtj2FOgl.js";import"./app.D7nie2tN.js";import"./chunks/dayjs.BdcnXKr1.js";import"./chunks/vue-i18n.-J8ISBuY.js";import"./chunks/pinia.ecF83nE_.js";/* empty css                    */import"./chunks/nprogress.B6x19dEb.js";import"./YunComment.vue_vue_type_style_index_0_lang.Bid5-W6J.js";import"./index.C5okkQwF.js";import"./YunPageHeader.vue_vue_type_script_setup_true_lang.BnNbMMRz.js";import"./post.CQaILt3k.js";const C=m("/posts/2025/about_recently_1",async i=>JSON.parse('{"title":"关于最近发生的事","description":"","frontmatter":{"title":"关于最近发生的事","date":"2025-10-28 19:25:00","updated":"2025-10-28 19:25:00","categories":["生活"],"tags":["博客","生活","未来"],"codeHeightLimit":300},"headers":[],"relativePath":"pages/posts/2025/about_recently_1.md","lastUpdated":1766588262000}'),{lazy:(i,t)=>i.name===t.name}),w={__name:"about_recently_1",setup(i,{expose:t}){const{data:r}=C(),f=g(),c=b(),o=Object.assign(c.meta.frontmatter||{},r.value?.frontmatter||{});return f.currentRoute.value.data=r.value,A("valaxy:frontmatter",o),globalThis.$frontmatter=o,t({frontmatter:{title:"关于最近发生的事",date:"2025-10-28 19:25:00",updated:"2025-10-28 19:25:00",categories:["生活"],tags:["博客","生活","未来"],codeHeightLimit:300}}),(e,a)=>{const u=d;return P(),E(u,{frontmatter:T(o)},{"main-content-md":l(()=>[a[0]||(a[0]=n("h1",{id:"关于最近",tabindex:"-1"},[s("关于最近 "),n("a",{class:"header-anchor",href:"#关于最近","aria-label":'Permalink to "关于最近"'},"​")],-1)),a[1]||(a[1]=n("h2",{id:"关于博客",tabindex:"-1"},[s("关于博客 "),n("a",{class:"header-anchor",href:"#关于博客","aria-label":'Permalink to "关于博客"'},"​")],-1)),a[2]||(a[2]=n("p",null,"起始从博客首页的小字就能知道最近博客发生了什么, 一开始是部署博客服务器提供商疑似跑路了(其实没跑, 不过是换了个域名), 拿到新域名后依然购买了服务器一个月, 但是并没有激活, 客服竟然公然劝我去举报他们公司…过了一段时间后补偿了一个服务器给我, 不过就没用过了. 不过公网IP似乎没有收回, 不过也用不了.",-1)),a[3]||(a[3]=n("p",null,"其实服务器跑路也没什么, 因为博客源代码都在电脑上, 只要有服务器就能马上部署, 之后你也能猜到了, 就是电脑被格了. (老实了, 现在我直接把幸存的代码全上传了)",-1)),a[4]||(a[4]=n("p",null,"这里我是真很想去质问小米的官方镜像, 原以为就是普通的重装, 结果直接把整个硬盘都格了(包括分配给Linux系统分区), 重装前还想着什么备份了, 什么还没备份, 哈哈哈, 所以为什么小米的工程师不写清楚, 这会格式化所有硬盘😡, 说是损失惨重吧, 感觉又没什么, 过去两三周也没什么感觉真正的消失了, 对了你一定很想问为什么我要格机. 请看下面",-1)),h(" more "),a[5]||(a[5]=n("div",{class:"language- max-h-300px"},[n("button",{title:"Copy code",class:"copy"}),n("span",{class:"lang"}),n("pre",{class:"shiki shiki-themes github-light github-dark vp-code"},[n("code",{"v-pre":""},[n("span",{class:"line"},[n("span",null,"Microsoft (R) Windows Debugger Version 6.12.0002.633 AMD64")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Copyright (c) Microsoft Corporation. All rights reserved.")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Loading Dump File [C:\\Users\\30789\\Desktop\\101525-10578-01.dmp]")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Mini Kernel Dump File: Only registers and stack trace are available")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Symbol search path is: srv*D:\\Symbol*https://msdl.microsoft.com/download/symbols")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Executable search path is: ")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Windows 7 Kernel Version 22621 MP (32 procs) Free x64")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Product: WinNt, suite: TerminalServer SingleUserTS Personal")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Machine Name:")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Kernel base = 0xfffff806`6f000000 PsLoadedModuleList = 0xfffff806`6fc13580")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Debug session time: Wed Oct 15 14:09:32.146 2025 (UTC + 8:00)")]),s(`
+`),n("span",{class:"line"},[n("span",null,"System Uptime: 0 days 0:04:08.827")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Loading Kernel Symbols")]),s(`
+`),n("span",{class:"line"},[n("span",null,"...............................................................")]),s(`
+`),n("span",{class:"line"},[n("span",null,"................................................................")]),s(`
+`),n("span",{class:"line"},[n("span",null,"................................................................")]),s(`
+`),n("span",{class:"line"},[n("span",null,"...........................................................")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Loading User Symbols")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Loading unloaded module list")]),s(`
+`),n("span",{class:"line"},[n("span",null,"....................")]),s(`
+`),n("span",{class:"line"},[n("span",null,"*******************************************************************************")]),s(`
+`),n("span",{class:"line"},[n("span",null,"*                                                                             *")]),s(`
+`),n("span",{class:"line"},[n("span",null,"*                        Bugcheck Analysis                                    *")]),s(`
+`),n("span",{class:"line"},[n("span",null,"*                                                                             *")]),s(`
+`),n("span",{class:"line"},[n("span",null,"*******************************************************************************")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Use !analyze -v to get detailed debugging information.")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"BugCheck 3B, {c0000005, fffff8066f2a6034, ffffa005550bd940, 0}")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"6: kd> !analyze -v")]),s(`
+`),n("span",{class:"line"},[n("span",null,"*******************************************************************************")]),s(`
+`),n("span",{class:"line"},[n("span",null,"*                                                                             *")]),s(`
+`),n("span",{class:"line"},[n("span",null,"*                        Bugcheck Analysis                                    *")]),s(`
+`),n("span",{class:"line"},[n("span",null,"*                                                                             *")]),s(`
+`),n("span",{class:"line"},[n("span",null,"*******************************************************************************")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"SYSTEM_SERVICE_EXCEPTION (3b)")]),s(`
+`),n("span",{class:"line"},[n("span",null,"An exception happened while executing a system service routine.")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Arguments:")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Arg1: 00000000c0000005, Exception code that caused the bugcheck")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Arg2: fffff8066f2a6034, Address of the instruction which caused the bugcheck")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Arg3: ffffa005550bd940, Address of the context record for the exception that caused the bugcheck")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Arg4: 0000000000000000, zero.")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Debugging Details:")]),s(`
+`),n("span",{class:"line"},[n("span",null,"------------------")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"EXCEPTION_CODE: (NTSTATUS) 0xc0000005 - 0x%p")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"FAULTING_IP: ")]),s(`
+`),n("span",{class:"line"},[n("span",null,"nt!RtlpUnwindPrologue+204")]),s(`
+`),n("span",{class:"line"},[n("span",null,"fffff806`6f2a6034 488b08          mov     rcx,qword ptr [rax]")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"CONTEXT:  ffffa005550bd940 -- (.cxr 0xffffa005550bd940)")]),s(`
+`),n("span",{class:"line"},[n("span",null,"rax=0000000000000000 rbx=0000000000000004 rcx=0000000000000000")]),s(`
+`),n("span",{class:"line"},[n("span",null,"rdx=ffffa005550bf110 rsi=ffffa005550be360 rdi=fffff8066f07e84c")]),s(`
+`),n("span",{class:"line"},[n("span",null,"rip=fffff8066f2a6034 rsp=ffffa005550be360 rbp=ffffa005550be680")]),s(`
+`),n("span",{class:"line"},[n("span",null," r8=000000000000000f  r9=ffffa005550be738 r10=0000000000000018")]),s(`
+`),n("span",{class:"line"},[n("span",null,"r11=fffff8066f000000 r12=00007fffffff0000 r13=00007ffffffeffff")]),s(`
+`),n("span",{class:"line"},[n("span",null,"r14=ffffa005550be6a0 r15=fffff8066f762d44")]),s(`
+`),n("span",{class:"line"},[n("span",null,"iopl=0         nv up ei ng nz ac po nc")]),s(`
+`),n("span",{class:"line"},[n("span",null,"cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00050296")]),s(`
+`),n("span",{class:"line"},[n("span",null,"nt!RtlpUnwindPrologue+0x204:")]),s(`
+`),n("span",{class:"line"},[n("span",null,"fffff806`6f2a6034 488b08          mov     rcx,qword ptr [rax] ds:002b:00000000`00000000=000000000001c200")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Resetting default scope")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"CUSTOMER_CRASH_COUNT:  1")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"DEFAULT_BUCKET_ID:  VISTA_DRIVER_FAULT")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"BUGCHECK_STR:  0x3B")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"PROCESS_NAME:  sppsvc.exe")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"CURRENT_IRQL:  0")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"BAD_PAGES_DETECTED: 1c200")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"LAST_CONTROL_TRANSFER:  from 0000000000000000 to fffff8066f2a6034")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"STACK_TEXT:  ")]),s(`
+`),n("span",{class:"line"},[n("span",null,"ffffa005`550be360 00000000`00000000 : 00000000`00000000 00000000`00000000 00000000`00000000 00000000`00000000 : nt!RtlpUnwindPrologue+0x204")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"SYMBOL_NAME:  PAGE_NOT_ZERO")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"FOLLOWUP_NAME:  MachineOwner")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"MODULE_NAME: Unknown_Module")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"IMAGE_NAME:  Unknown_Image")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"DEBUG_FLR_IMAGE_TIMESTAMP:  0")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"STACK_COMMAND:  .cxr 0xffffa005550bd940 ; kb")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"BUCKET_ID:  PAGE_NOT_ZERO")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Followup: MachineOwner")]),s(`
+`),n("span",{class:"line"},[n("span",null,"---------")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null," *** Memory manager detected 115200 instance(s) of page corruption, target is likely to have memory corruption.")]),s(`
+`),n("span",{class:"line"},[n("span",null," ")]),s(`
+`),n("span",{class:"line"},[n("span",null,"6: kd> !process")]),s(`
+`),n("span",{class:"line"},[n("span",null,"PROCESS ffff85053250f080")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    SessionId: none  Cid: 2e54    Peb: eb391f4000  ParentCid: 05d0")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    DirBase: 2d065d000  ObjectTable: ffffb38b1ba95040  HandleCount: <Data Not Accessible>")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    Image: sppsvc.exe")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    VadRoot ffff850531c49e40 Vads 1 Clone 0 Private 721. Modified 3. Locked 16.")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    DeviceMap ffffb38b0eb637ff")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    Token                             ffffb38b1c2450a0")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    ReadMemory error: Cannot get nt!KeMaximumIncrement value.")]),s(`
+`),n("span",{class:"line"},[n("span",null,"fffff78000000000: Unable to get shared data")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    ElapsedTime                       00:00:00.000")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    UserTime                          00:00:00.000")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    KernelTime                        00:00:00.000")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    QuotaPoolUsage[PagedPool]         0")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    QuotaPoolUsage[NonPagedPool]      0")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    Working Set Sizes (now,min,max)  (0, 0, 0) (0KB, 0KB, 0KB)")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    PeakWorkingSetSize                0")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    VirtualSize                       2101333 Mb")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    PeakVirtualSize                   2101333 Mb")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    PageFaultCount                    0")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    MemoryPriority                    BACKGROUND")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    BasePriority                      8")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    CommitCharge                      850")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"        *** Error in reading nt!_ETHREAD @ ffff85052cee1080")])])]),n("button",{class:"code-block-unfold-btn"})],-1)),a[6]||(a[6]=n("p",null,[s("还挺长的, 就直接折叠了, 简单来说似乎是"),n("code",null,"sppsvc.exe"),s("访问非法内存导致的, 不过我在很多软件都能遇到这种非法内存访问的, 也不清楚到底是内存条的问题还是系统的问题(反正重装后目前还没出现问题), 所以就想着重装能不能解决.")],-1)),a[7]||(a[7]=n("h2",{id:"关于生活",tabindex:"-1"},[s("关于生活 "),n("a",{class:"header-anchor",href:"#关于生活","aria-label":'Permalink to "关于生活"'},"​")],-1)),a[8]||(a[8]=n("p",null,"因为来回科协麻烦, 平时不在科协也有一定大屏幕查看代码和网页的需求, 所以就斥巨资买了平板, 其实当时还在犹豫到底买不买, 便宜的平板小新, 不过系统太拉, 而小米又太贵, 结果恰好得知小米Pad8要出了, 而且首发(10月8号前)购买的大学生又笔和套拿, 说实话本来还在犹豫, 听到有笔直接拿下(被小米拿下了呜😴, 不过一支笔官网要卖599, 感觉还行其实), 再加上国补, 就拿下了Pad8, 目前体验下来感觉大屏幕就是爽(刷视频和浏览网页这一块确实是手机无法比的), 性能也很能说的过去(晓龙8sGen4, 别吐糟是8s, 但是比手机的7+Gen2要好)",-1)),a[9]||(a[9]=n("h2",{id:"关于就业和未来",tabindex:"-1"},[s("关于就业和未来 "),n("a",{class:"header-anchor",href:"#关于就业和未来","aria-label":'Permalink to "关于就业和未来"'},"​")],-1)),a[10]||(a[10]=n("p",null,[s("这个感觉是真的是我犹豫很久无法给出的答案, 目前想着本科就业, 但是却在软件和硬件方面纠结. 两边的招聘信息也都看过, 感觉上自己需要学习的东西很有很多, 不过自己在爱好上可能更偏向软件开发一点, 说实话, 我是真想知道现在的就业市场是怎样的, 到底是不是那种供大于求的状态, 还是计算机会沦落为第二个土木. 在写这篇博客时其实我还在犹豫, 感觉自己站在人生的岔路口, 但是不知道向哪里走, 或许, 在以后的"),n("code",null,"about recently"),s("能给出我的答案.")],-1))]),"main-header":l(()=>[p(e.$slots,"main-header")]),"main-header-after":l(()=>[p(e.$slots,"main-header-after")]),"main-nav":l(()=>[p(e.$slots,"main-nav")]),"main-content-before":l(()=>[p(e.$slots,"main-content-before")]),"main-content":l(()=>[p(e.$slots,"main-content")]),"main-content-after":l(()=>[p(e.$slots,"main-content-after")]),"main-nav-before":l(()=>[p(e.$slots,"main-nav-before")]),"main-nav-after":l(()=>[p(e.$slots,"main-nav-after")]),comment:l(()=>[p(e.$slots,"comment")]),footer:l(()=>[p(e.$slots,"footer")]),aside:l(()=>[p(e.$slots,"aside")]),"aside-custom":l(()=>[p(e.$slots,"aside-custom")]),default:l(()=>[p(e.$slots,"default")]),_:3},8,["frontmatter"])}}};export{w as default,C as usePageData};
